08366b66bdd38990cefcab2c81179f5a12d8ecd9
Ken Dale <ken@kendaleiv.com>
2015-08-26 12:47:38 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Automation : Azure Storage Emulator
+
+Programmatically control Azure Storage Emulator.
+
+## Thanks
+
+Thanks to [Ritter IM](http://ritterim.com) for supporting OSS.
---
+++ src/Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Automation.StorageEmulator")]
+[assembly: AssemblyProduct("Automation.StorageEmulator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("Ritter Insurance Marketing, LLC")]
+[assembly: AssemblyCopyright("Copyright © 2015 Ritter Insurance Marketing, LLC")]
+#if !DEBUG
+[assembly: AssemblyConfiguration("Release")]
+#else
+[assembly: AssemblyConfiguration("Debug")]
+#endif
+
+[assembly: ComVisible(false)]
